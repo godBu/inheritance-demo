23b3d70bfbc9b9a82c9823e2d723b4f735bd64a9
Ebuka Mbata <rockymmpr@gmail.com>
2020-06-19 12:01:33 +0100

    vehicle.cs

---
+++ inheritance-demo/Vehicle.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace inheritance_demo
+{
+    public class Vehicle // base class / parent class
+    {
+        public int doors;
+        public double hP;
+        public string color;
+        public string regPlate;
+        public bool wowFactor;
+        public bool insurance;
+        public bool tax;
+        public int wheels;
+
+        public void beep()
+        {
+            Console.WriteLine("Beep");
+            Console.Beep();
+        }
+    }
+
+    public class Car : Vehicle // child class / derived class
+    {
+        public string brand;
+        public string model;
+        public bool boot;
+    }
+
+    // here...
+
+    public class Motorbike : Vehicle
+    {
+        public string brand;
+        public bool handlebars;
+        public double cc;
+
+        public Motorbike(int Doors, string Brand, bool Handlebars, double CC)
+        {
+            doors = Doors;
+            brand = Brand;
+            handlebars = Handlebars;
+            cc = CC;
+        }
+    }
+
+}
